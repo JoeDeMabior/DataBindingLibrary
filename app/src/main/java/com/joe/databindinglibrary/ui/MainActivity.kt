09c19c799ba598c170ecd0b0bdfad9f58595d7e8
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-05-08 21:23:06 +0300

    Create a data binding object

---
+++ app/src/main/java/com/joe/databindinglibrary/ui/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.joe.databindinglibrary.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.joe.databindinglibrary.R
+import com.joe.databindinglibrary.databinding.ActivityMainBinding
+import com.joe.databindinglibrary.model.Contact
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val contact = Contact("Joe", "Deng")
+        binding.contact = contact
+    }
+}
