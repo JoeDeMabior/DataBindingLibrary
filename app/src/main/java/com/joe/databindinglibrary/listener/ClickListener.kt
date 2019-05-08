d406e1c677c3dc7e5795abce16985d91f2569787
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-05-08 22:06:08 +0300

    Handle long click listener

--- app/src/main/java/com/joe/databindinglibrary/listener/ClickListener.kt
+++ app/src/main/java/com/joe/databindinglibrary/listener/ClickListener.kt
@@ -9,4 +9,9 @@ class ClickListener(var context: Context) {
     fun simpleButtonClick(view: View) {
         Toast.makeText(context, "Simple Button Click.", Toast.LENGTH_SHORT).show()
     }
+
+    fun simpleButtonLongClick(view: View): Boolean {
+        Toast.makeText(context, "Simple Button Long Click.", Toast.LENGTH_SHORT).show()
+        return false
+    }
 }
