011b3d68b263a5f05fec25fa2ecadbe120890f86
JoeDeMabior <39078169+JoeDeMabior@users.noreply.github.com>
2019-05-08 22:20:29 +0300

    Handle click with parameters

--- app/src/main/java/com/joe/databindinglibrary/listener/ClickListener.kt
+++ app/src/main/java/com/joe/databindinglibrary/listener/ClickListener.kt
@@ -3,6 +3,7 @@ package com.joe.databindinglibrary.listener
 import android.content.Context
 import android.view.View
 import android.widget.Toast
+import com.joe.databindinglibrary.model.Contact
 
 class ClickListener(var context: Context) {
 
@@ -13,5 +14,12 @@ class ClickListener(var context: Context) {
     fun simpleButtonLongClick(view: View): Boolean {
         Toast.makeText(context, "Simple Button Long Click.", Toast.LENGTH_SHORT).show()
         return false
+    }
+
+    fun simpleButtonClickWithParams(view: View, contact: Contact) {
+        Toast.makeText(context, "Simple Button Click With Parameters.\n" +
+                "First Name: ${contact.firstName}\n" +
+                "Last Name: ${contact.lastName}",
+            Toast.LENGTH_SHORT).show()
     }
 }
